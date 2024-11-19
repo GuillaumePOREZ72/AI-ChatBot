e43cbba2224afc3658de312e1a01e9b4da5c0a00
GuillaumePOREZ72 <guillaume.porez@oclock.school>
2024-11-19 17:10:48 +0100

    first commit

---
+++ src/components/ChatBotStart.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prop-types */
+import "./ChatBotStart.css";
+
+const ChatBotStart = ({ onStartChat }) => {
+  return (
+    <div className="start-page">
+      <button className="start-page-btn" onClick={onStartChat}>
+        Chat AI
+      </button>
+    </div>
+  );
+};
+
+export default ChatBotStart;
